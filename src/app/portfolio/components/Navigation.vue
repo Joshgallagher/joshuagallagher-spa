9cae78857834a78d4eb6f795dfe5ac96ae2ad18e
Joshua Gallagher <joshuagallagher@Joshuas-MacBook-Pro.local>
2017-10-31 20:09:13 +0000

    Added some assets, created sass vars and created Footer component.

---
+++ src/app/portfolio/components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navigation">
+
+  </nav>
+</template>
+<script>
+  export default {
+    name: 'navigation'
+  }
+</script>
+<style lang="sass">
+  @import '~sass/main'
+
+  .navigation
+</style>
--- src/app/portfolio/components/Portfolio.vue
+++ src/app/portfolio/components/Portfolio.vue
@@ -1,9 +1,19 @@
 <template>
-  <p>Portfolio</p>
+  <div class="portfolio">
+    <navigation></navigation>
+    <page-end></page-end>
+  </div>
 </template>
 <script>
+  import navigation from './Navigation'
+  import pageEnd from '@/app/global/components/PageEnd'
+
   export default {
-    name: 'portfolio'
+    name: 'portfolio',
+    components: {
+      navigation,
+      pageEnd
+    }
   }
 </script>
 <style lang="sass">

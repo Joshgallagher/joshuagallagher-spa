20d3aac0f70b74c4427dfff9bbba172dbeb42913
Joshua Gallagher <joshuagallagher@Joshuas-MacBook-Pro.local>
2017-11-06 15:18:45 +0000

    Refactored page end component

--- src/app/global/components/PageEnd.vue
+++ src/app/global/components/PageEnd.vue
@@ -5,7 +5,8 @@
       <div class="footer__links">
         <a href="https://github.com/Joshgallagher" target="blank" class="footer__link">GitHub</a>
         <a href="https://www.linkedin.com/in/joshua-gallagher-616653127" target="blank" class="footer__link">LinkedIn</a>
-        <router-link v-if="" :to="{name: 'blog' }" class="footer__link">Blog</router-link>
+        <router-link v-if="this.$route.name === 'portfolio'" :to="{name: 'blog' }" class="footer__link">Blog</router-link>
+        <router-link v-else :to="{name: 'portfolio' }" class="footer__link">Portfolio</router-link>
       </div>
     </div>
   </footer>

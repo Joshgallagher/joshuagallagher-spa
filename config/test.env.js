99000e7f48910fab74046f7078dc3a3bf3a4bddb
Joshua Gallagher <joshgallagher24@gmail.com>
2017-12-22 17:47:01 +0000

    Removed api url from test config

--- config/test.env.js
+++ config/test.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"',
-  API_URL: '"http://joshuagallagher-api.test"'
+  NODE_ENV: '"testing"'
 })

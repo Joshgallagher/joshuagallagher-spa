42f9cbb8af6cfc818cad68349a4fc8f05f27aecb
Joshua Gallagher <joshgallagher24@gmail.com>
2018-01-31 23:26:02 +0000

    Updated portfolio component - removed raw JSON data and stored it in a data file.

---
+++ src/app/portfolio/data/projects.js
@@ -0,0 +1,34 @@
+export default {
+  projects: {
+    wip: [
+      {
+        'id': 1,
+        'name': 'Stack',
+        'url': 'https://github.com/Joshgallagher/stack',
+        'status': 'Completed',
+        'date': 'January 2018',
+        'platforms': 'Website',
+        'about': 'Stack is re-make of Ketchapp\'s iOS and Android game Stack. The motivation behind this project was sparked from my Computer Graphics module as University and wanting to experiment with graphics in the browser with JavaScript. The technologies used in this project are as follows: Webpack, three.js and GSAP.'
+      },
+      {
+        'id': 2,
+        'name': 'joshuagallagher.io',
+        'url': '#',
+        'status': 'Completed',
+        'date': 'November 2017',
+        'platforms': 'Website',
+        'about': 'I decided that my personal brand needed a revamp - in the direction of securing a placement. To achieve this, I created both a portfolio and blog. The idea was to show employers relevant information about myself but, also show that I\'m active in learning new technologies and showcasing this through my blog. My personal site was built with Lumen, Node.js and Vue.js'
+      },
+      {
+        'id': 3,
+        'url': '#',
+        'name': 'short.en',
+        'status': 'Ongoing',
+        'date': 'October 2017',
+        'platforms': 'Website',
+        'about': 'short.en is a URL shortener built with Lumen, Node.js and Vue.js. short.en takes a URL specified by a user and returns a shortened version for use across the web. short.en stores all statistics associated with a URL - how many times it has been shortened, when it was first shortened and when it was last shortened. Upon completion of this project, short.en will be hosted through Heroku.'
+      }
+    ],
+    coming: []
+  }
+}

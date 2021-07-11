1e3e14e6f6d001d1865826b605663ed37a3418dc
Joshua Gallagher <joshgallagher24@gmail.com>
2021-07-11 18:18:02 +0100

    refactor: remove project

--- src/app/portfolio/data/projects.js
+++ src/app/portfolio/data/projects.js
@@ -3,24 +3,15 @@ export default {
     wip: [
       {
         'id': 1,
-        'name': 'Weld',
-        'url': 'https://www.weldapp.co/',
-        'status': 'Completed',
-        'date': 'October 2020',
-        'platforms': 'Website',
-        'about': 'Responsible for developing Welds\' MVP product to help them prove market fit. With a core focus on geo-based search and back-end data processing to help drivers find mechanics.'
-      },
-      {
-        'id': 2,
         'name': 'Stack',
         'url': 'https://github.com/Joshgallagher/stack',
         'status': 'Completed',
         'date': 'January 2018',
         'platforms': 'Website',
-        'about': 'Stack is a re-make of Ketchapp\'s iOS and Android game Stack. The motivation behind this project was sparked from my Computer Graphics module at University and wanting to experiment with graphics in the browser. The technologies used in this project are as follows: Webpack, three.js and GSAP.'
+        'about': 'Stack is a re-make of Ketchapp\'s iOS and Android game Stack. The technologies used in this project are as follows: Webpack, three.js and GSAP.'
       },
       {
-        'id': 3,
+        'id': 2,
         'name': 'joshuagallagher.io',
         'url': '#',
         'status': 'Completed',
@@ -29,7 +20,7 @@ export default {
         'about': 'Personal website rebrand built with Lumen, Node.js and Vue.js'
       },
       {
-        'id': 4,
+        'id': 3,
         'url': '#',
         'name': 'Shorten',
         'status': 'Completed',
